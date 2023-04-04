21ece220b5ac6bb75f4d11c970c16a4ae77242c7
Lucas Kristandt <lucas.kristandt@pinuts.de>
2023-04-04 13:34:02 +0200

    use process env

--- App/src/Requests/bookSearch.request.js
+++ App/src/Requests/bookSearch.request.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-import { GOOGLE_BOOKS_API_KEY } from '../env-variables';
-
 const bookSearchRequest = (searchParams) =>
-    axios.get(`https://www.googleapis.com/books/v1/volumes?key=${GOOGLE_BOOKS_API_KEY}`, {
-        params: searchParams,
-    });
+    axios.get(
+        `https://www.googleapis.com/books/v1/volumes?key=${process.env.GOOGLE_BOOKS_API_KEY}`,
+        {
+            params: searchParams,
+        }
+    );
 
 export default bookSearchRequest;
